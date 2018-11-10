6523829ceeaf9faed37418749f4d1c24efdd4ff2
Emily Lauren Roth <emilylroth@utexas.edu>
2018-11-10 15:08:00 -0500

    Updated to have the correct thing for reading in graph from a file

--- main.cu
+++ main.cu
@@ -1,28 +1,36 @@
 #include <stdio.h>
 #include <fstream>
+#include <iostream>
+#include <stdlib.h>
 
-int main(int argc, char **argv){
+using namespace std;
+
+int main(){
 
 	// make an undirected and connected graph
 	// remember n is vertices and m is edges
-	FILE* pFile = fopen("input.txt", "r")
-	if(ptr == NULL){
-		printf("No such file fam wyd");
+	cout << "hello world";
+	FILE* pFile = fopen("input.txt", "r");
+	if(pFile == NULL){
+		cout << "Fam u can't do that stop";
 		return 0;
 	}
 	// read in first line with n and m
 	int n, m;
 	fscanf(pFile, "%d %d", &n, &m);
-	printf("%d %d\n", n, m);
+	cout << "Settings: " << n << " " << m << endl;
 	int adj_mat[n][n]; // idk what values we want but I'm starting with 4 nodes
-
 	// iterate thru all edges
 	for(int i = 0; i<m; i++){
 		int n1, n2, weight;
 		fscanf(pFile, "%d %d %d", &n1, &n2, &weight);
 		adj_mat[n1][n2] = weight; // put that node in
-		printf("%d\n", adj_mat[n1][n2]);
-	}
+		adj_mat[n2][n1] = weight; //gotta put it in for both
+	} 
+
+	// adjacency graph built now it's dumb lit
+	// okay fam lets get this bread with the normal sequential algorithm
+
 
 	return 0;
 }
