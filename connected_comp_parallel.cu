a9cad25a6af4238467ce1d6da020f17a2cd293f5
KevinLoisel-Perilla <kperilla@utexas.edu>
2018-11-14 21:12:37 -0600

    it works!

--- connected_comp_parallel.cu
+++ connected_comp_parallel.cu
@@ -3,12 +3,29 @@
 #include <iostream>
 #include <stdlib.h>
 
-__global__ void befriend_adjacents(int** adj_lists, int* sizes, int* labels, int* changed) {
+using namespace std;
+
+__global__ void befriend_adjacents(int* adj_lists, int* sizes, int* labels, int* changed) {
     int id = threadIdx.x;
-    int* list = adj_lists[id];
+
+    // Get start of adj list
+    int begin = 0;
+    for (int i = 0; i < id; i++) {
+        begin += sizes[i];
+    }
+
+    int* list = &adj_lists[begin];
+
+    // debug
+    // if (sizes[id] == 0) {
+    //     debug[id] = -1;
+    // } else {
+    //     debug[id] = list[0];
+    // }
+
     int min = labels[id];
     for (int i = 0; i < sizes[id]; i++) {
-        int adj_label = labels[list[i]]
+        int adj_label = labels[list[i]];
         if (adj_label < min) {
             min = adj_label;
             *changed = 1;
@@ -19,25 +36,60 @@ __global__ void befriend_adjacents(int** adj_lists, int* sizes, int* labels, int
 
 __global__ void init_labels(int* labels) {
     int id = threadIdx.x;
-    label[id] = id;
+    labels[id] = id;
 }
 
-void cc_para(int** adj_lists, int size, int* list_sizes) {
+void print_labels(int* labels, int size) {
+    cout << "labels: ";
+    for (int i = 0; i < size; i++) {
+        cout << labels[i] << " ";
+    }
+    cout << endl;
+}
+
+void print_lists(int* adj_lists, int size, int* list_sizes) {
+    cout << "adjacency lists:" << endl;
+    int* list = adj_lists;
+    for (int i = 0; i < size; i++) {
+        cout << "[";
+        for (int j = 0; j < list_sizes[i]; j++) {
+            cout << " " << list[j] << " ";
+        }
+        cout << "]" << endl;
+        list += list_sizes[i];
+    }
+}
+// void print_debug(int* debug, int size) {
+//     cout << "debug: ";
+//     for (int i = 0; i < size; i++) {
+//         cout << debug[i] << " ";
+//     }
+//     cout << endl;
+// }
+
+// adj_lists is flattened to 1d array
+void cc_para(int* adj_lists, int size, int* list_sizes) {
     int* labels = (int*)malloc(size * sizeof(int));
     int* d_labels;
+
+    int list_size_total = 0;
+    for (int i = 0; i < size; i++) {
+        list_size_total += list_sizes[i];
+    }
+
     cudaMalloc((void**)&d_labels, size * sizeof(int));
 
-    init_labels<<<1, size>>>(d_labels)
+    init_labels<<<1, size>>>(d_labels);
 
-    int** d_adj_lists;
-    cudaMalloc((void**)&d_adj_lists, size * sizeof(int*));
-    for (int i = 0; i < size; i++) {
-        cudaMalloc((void**)d_adj_lists[i], list_sizes[i] * sizeof(int));
-        cudaMemcpy(*(&d_adj_lists[i]), &adj_lists[i], list_sizes[i] * sizeof(int), cudaMemcpyHostToDevice);
-    }
+    cudaMemcpy(labels, d_labels, size * sizeof(int), cudaMemcpyDeviceToHost);
+
+    int* d_adj_lists;
+    cudaMalloc((void**)&d_adj_lists, list_size_total * sizeof(int));
+    cudaMemcpy(d_adj_lists, adj_lists, list_size_total * sizeof(int), cudaMemcpyHostToDevice);
+
     int* d_sizes;
     cudaMalloc((void**)&d_sizes, size * sizeof(int));
-    cudaMemcpy(d_sizes, sizes, size * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_sizes, list_sizes, size * sizeof(int), cudaMemcpyHostToDevice);
 
     int* changed = (int*)malloc(sizeof(int));
     int* d_changed;
@@ -55,25 +107,27 @@ void cc_para(int** adj_lists, int size, int* list_sizes) {
 
     cudaMemcpy(labels, d_labels, size * sizeof(int), cudaMemcpyDeviceToHost);
 
-    for (int i = 0; i < size; i++) {
-        cudaFree(d_adj_lists[i]);
-    }
+    print_labels(labels, size);
+
     cudaFree(d_changed); cudaFree(d_labels); cudaFree(d_changed); cudaFree(d_adj_lists);
 
     free(labels); free(changed);
 }
 
 int main() {
-    int * adj_lists[3];
-    int list0[0] = [];
-    int list1[1] = [2];
-    int list2[2] = [1]; 
-    adj_lists[0] = list0;
-    adj_lists[1] = list1;
-    adj_lists[2] = list2;
+    // int * adj_lists[3];
+    // int* list0;
+    // int list1[1] = {2};
+    // int list2[2] = {1}; 
+    // adj_lists[0] = list0;
+    // adj_lists[1] = list1;
+    // adj_lists[2] = list2;
+    int adj_lists[4] = {1, 2, 0, 0};
 
-    int size = 3;
-    int sizes[3] = {0, 1, 1};
+    int size = 4;
+    int sizes[4] = {2, 1, 1, 0};
+
+    print_lists(adj_lists, size, sizes);
 
     cc_para(adj_lists, size, sizes);
 }
