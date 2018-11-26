ec8c94502a610d47e8724bb62ed83742cfcd17f0
Turan Vural <tzvural@utexas.edu>
2018-11-25 21:21:59 -0600

    Now it actually works - fixed adj. list traversal

--- ConnectedCompSequential.java
+++ ConnectedCompSequential.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class ConnectedCompSequential {
 
 	//test 1, matching cu files
@@ -7,31 +9,40 @@ public class ConnectedCompSequential {
 
 	static boolean [] visited = {false,false,false,false,false,false,false,false,false,false,false};
 
+	static int [] output = new int[vertices];
+
 	public static void main (String [] args) {
+
 		int count = 0;
 		while(!checkDone()) {
+			System.out.println("" + Arrays.toString(visited));
+
 			int search = 0;
 			for(int i = 0; i < visited.length; i++) {
 				if(!visited[i]) {
-					search = i;
+					search = i; break;
 				}
 			}
-
-			doDFS(search);
+			System.out.println("" + search);
+			doDFS(search, search);
 			count ++;
 		}
-		System.out.println("" + count);
+		System.out.println("" + count + " " + Arrays.toString(output));
 	}
 
-	public static void doDFS(int v) {
+	public static void doDFS(int v, int color) {
 		// label v as discovered
 		visited[v] = true;
+		output[v] = color;
 		// for all edges from v to w in G.adjacentEdges(v) do
-		for (int i = 0; i < sizes[v]; i++)
+		for (int i = 0; i < sizes[v]; i++) {
 			// if vertex w is not labeled as discovered then
-			if(!visited[adj_lists[v + i]])
+			if(!visited[adj_lists[getOffset(v) + i]]) {
 				// recursively call DFS(G,w)
-				doDFS(adj_lists[v + i]);
+				doDFS(adj_lists[getOffset(v) + i], color);
+
+			}
+		}
 	}
 
 	public static boolean checkDone() {
@@ -39,5 +50,13 @@ public class ConnectedCompSequential {
 			if(!node)
 				return node;
 		return true;
+	}
+
+	public static int getOffset(int node) {
+		int offset = 0;
+		for (int i = 0; i < node; i++) {
+			offset += sizes[i];
+		}
+		return offset;
 	}
 }
